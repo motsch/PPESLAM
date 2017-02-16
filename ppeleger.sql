ee840ee1091d551b481b1f13c6dc75ab3a564623
francis <francis.motsch@gmail.Com>
2017-02-16 14:24:28 +0100

    16/02/2017
    Fin de l'étape de préparation du projet. Début de la programmation.

---
+++ ppeleger.sql
@@ -0,0 +1,113 @@
+drop database if exists ppeleger;
+Create database ppeleger;
+use ppeleger;
+
+
+Create table saison
+(
+IDsaison int(1) NOT NULL,
+nomSaison varchar(10) NOT NULL,
+PRIMARY KEY(IDsaison)
+);
+
+
+Create table propri
+(
+IDPropri int(9) NOT NULL,
+nomPropri varchar(30) NOT NULL,
+prenomPropri varchar(30) NOT NULL,
+adresPropri varchar(30) NOT NULL,
+CPropri int(5) NOT NULL,
+villPropri varchar(25) NOT NULL,
+telPropri varchar(13) NOT NULL,
+mailPropri varchar(50) NOT NULL,
+PRIMARY KEY(IDPropri)
+);
+
+
+Create table Lieu
+(
+IDLieu int(6) NOT NULL,
+DescripLieu varchar(200),
+prixMin int(6),
+prixMax int(6),
+PRIMARY KEY(IDLieu)
+);
+
+
+Create table appartement
+(
+IDAppart int(6) NOT NULL,
+nomAppartement varchar(30),
+numImmeuble varchar(5),
+adresAppart varchar(20) NOT NULL,
+CPAppart int(5) NOT NULL,
+villAppart varchar(20) NOT NULL,
+dispoDebut date,
+dispoFin date,
+type char(2) NOT NULL,
+prix int(6) NOT NULL,
+/*photo varbinary(max) NOT NULL,     Le type pour les photos?*/ 
+PRIMARY KEY(IDAppart)
+);
+
+
+Create table Client
+(
+IDClient int(6) NOT NULL,
+civilite enum (‘madame’, ‘monsieur’),
+nomClient varchar(30) NOT NULL,
+prenomClient varchar(30) NOT NULL,
+adresClient varchar(20) NOT NULL,
+CPClient int(5) NOT NULL,
+telClient varchar(10) NOT NULL,
+mail varchar(50) NOT NULL,
+PRIMARY KEY(IDClient)
+);
+
+
+Create table Equipement
+(
+IDEquip int(6) NOT NULL,
+nomEquip varchar(30) NOT NULL,
+descripEquip varchar(250) NOT NULL,
+tarif float(4) NOT NULL,
+IDClient int(6) NOT NULL,
+PRIMARY KEY(IDEquip),
+FOREIGN KEY(IDClient) references Client(IDClient)
+);
+
+
+
+Create table Reservation
+(
+IDReserv int(9) NOT NULL,
+dateReserv date,
+descripReserv varchar(20),
+montant float(9),
+payee boolean,
+bloquee boolean
+);
+
+
+
+Create table majorer
+(
+IDsaison int(1) NOT NULL,
+IDAppart int(6) NOT NULL,
+majoPrix float(2),
+PRIMARY KEY(IDSaison, IDAppart),
+FOREIGN KEY(IDSaison) references saison(IDSaison),
+FOREIGN KEY(IDAppart) references appartement(IDAppart)
+);
+
+
+
+Create table appartenir
+(
+IDPropri int(9) NOT NULL,
+IDAppart int(6) NOT NULL,
+PRIMARY KEY(IDPropri, IDAppart),
+FOREIGN KEY(IDPropri) references propri(IDPropri),
+FOREIGN KEY(IDAppart) references appartement(IDAppart)
+);
